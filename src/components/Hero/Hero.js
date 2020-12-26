5b06472b603e32e6dc59588fb03646dd007f5384
Borja Lagarón <borjalagaron@gmail.com>
2020-12-26 22:14:14 +0100

    -i18n configured

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './Hero.css';
+import { useTranslation } from 'react-i18next';
 
 const Hero = (props) => {
   //   const width = window.innerWidth;
   //   const height = window.innerHeight;
 
+  const { t } = useTranslation();
+
   return (
     <div className="hero">
       <h1 className="hero-text hero-title">
-        Hola, I'm <span className="underlined underlined--offset">Borja</span>!
+        {t('hero.hello')}{' '}
+        <span className="underlined underlined--offset">Borja</span>!
       </h1>
-      <h2 className="hero-text hero-undertitle">
-        Though it may seem daunting it’s super easy! It’s pronounced /bohr-hah/
-      </h2>
+      <h2 className="hero-text hero-undertitle">{t('hero.pronuntiation')}</h2>
       <h3 className="hero-text hero-description">
-        I am a <span className="underlined underlined--offset">Product Owner</span> and Agile Enthusiast based in Stuttgart. 7+ years
-        in a Software Development Environment and also experienced as a
-        Developer. Sometimes I also enjoy writing, singing and taking long walks
-        through the forest.
+        {t('hero.desc1')}{' '}
+        <span className="underlined underlined--offset">
+          {t('hero.productowner')}
+        </span>{' '}
+        {t('hero.desc2')}
       </h3>
-
+      <h1>{t('alvaro')}</h1>
       {/* <h2>Let's play with the thickness of this underline</h2> */}
       {/* <p>
         <span className="underlined underlined--offset">
