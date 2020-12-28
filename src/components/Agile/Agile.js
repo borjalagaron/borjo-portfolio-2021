4410e6a67d00b56bb22954691cbb6b8b31eb0837
Borja Lagarón <borjalagaron@gmail.com>
2020-12-28 19:35:52 +0100

    -React Router implemented

---
+++ src/components/Agile/Agile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Agile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Agile Page</h1>
+      </div>
+    );
+  }
+}
+export default Agile;
---
+++ src/components/Frontend/Frontend.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Frontend extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Frontend Page</h1>
+      </div>
+    );
+  }
+}
+export default Frontend;
