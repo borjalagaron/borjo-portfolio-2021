eaac935d294251f67d53ecea74bb573d913955f7
Borja Lagarón <borjalagaron@gmail.com>
2021-01-11 03:52:43 +0100

    -imports deleted

--- src/Frontend/Frontend.jsx
+++ src/Frontend/Frontend.jsx
@@ -1,8 +1,5 @@
 import React, { lazy } from 'react';
-import { Route, Link } from 'react-router-dom';
 import ProjectCard from './components/ProjectCard';
-import Breadcrumbs from './Breadcrumbs';
-import paths from '../paths';
 
 // import weatherAppThumbnail from '%PUBLIC_URL%/images/thumbnails/WeatherApp';
 
@@ -38,12 +35,12 @@ const Frontend = ({ match }) => {
 
   return (
     <div className="Frontend">
-        {subRoutes.map((subRoute) => (
-          <>
-            <ProjectCard match={match} subRoute={subRoute} />
-          </>
-        ))}
-      </div>
+      {subRoutes.map((subRoute) => (
+        <>
+          <ProjectCard match={match} subRoute={subRoute} />
+        </>
+      ))}
+    </div>
   );
 };
 
